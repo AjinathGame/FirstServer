c9dbccdcd13a80edbb83826931528e55698e368e
Ajinath-Game <gameajinath4@gmail.com>
2025-09-16 19:31:54 +0530

    learn methods

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import todo from "./call.js";
 
 const app = express();
 const PORT = 2000;
@@ -6,6 +7,15 @@ const PORT = 2000;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.get("/todo", (req, res) =>{
+  res.json({message : "Todo List",data: todo})
+})
+
+app.post("/addtodo",(req,res) =>{
+  res.json({message : "Todo Added"})
+
+})
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
